fd5019e1294bfd0cfeec4c4b7140a178389b2d88
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-27 09:50:15 +1300

    Update gdevd.rs

--- src/bin/gdevd.rs
+++ src/bin/gdevd.rs
@@ -8,7 +8,6 @@ use std::time::Duration;
 
 use dbus::blocking::LocalConnection;
 use dbus::MethodErr;
-use dbus_tree;
 use dbus_tree::{Factory, Interface, MTFn};
 
 use gdev::Command::{Breathe, ColorSector, Cycle, Wave};
