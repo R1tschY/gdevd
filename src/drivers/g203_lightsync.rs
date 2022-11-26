647b68b9baa796f738e0cab07b4561cf5ee47bfc
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-27 09:57:36 +1300

    Update g203_lightsync.rs

--- src/drivers/g203_lightsync.rs
+++ src/drivers/g203_lightsync.rs
@@ -15,8 +15,9 @@ const DEFAULT_DIRECTION: Direction = Direction::RightToLeft;
 
 const DEVICE: DeviceDescription = DeviceDescription {
     product_id: 0xc092,
-    min_speed: 1000,
-    default_speed: Speed(10000),
+    min_speed: Speed(1000),
+    default_speed: Speed(10000), // 11000 ???
+    max_speed: Speed(20000),
     min_dpi: Dpi(50),
 };
 
